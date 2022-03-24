206167e4cc6bf362b8e98e193b4ecfecca600e86
jlogtenberg <j.logtenberg@student.rug.nl>
2022-03-24 11:54:59 +0100

    First lines of code

---
+++ main.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# other imports
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.layers import GlobalMaxPooling2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model
+
+# Load in the data
+cifar10 = tf.keras.datasets.cifar10
+
+# Distribute it to train and test set
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
