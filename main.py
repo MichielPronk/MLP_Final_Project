b928e97d857e2906e23f9783e15cafe406846aed
jlogtenberg <j.logtenberg@student.rug.nl>
2022-03-24 12:46:07 +0100

    Version 3

--- main.py
+++ main.py
@@ -1,60 +1,117 @@
-import tensorflow as tf
-import PIL
-
-# other imports
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
-from tensorflow.keras.layers import GlobalMaxPooling2D, MaxPooling2D
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.models import Model
+#%matplotlib inline
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Load in the data
-cifar10 = tf.keras.datasets.cifar10
 
-# Distribute it to train and test set
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# number of classes
-K = len(set(y_train))
+print(f"X_train shape: {X_train.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"X_test shape: {X_test.shape}")
+print(f"y_test shape: {y_test.shape}")
 
-# calculate total number of classes
-# for output layer
-print("number of classes:", K)
+# Scale the data
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-# Build the model using the functional API
-# input layer
-i = Input(shape=x_train[0].shape)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(i)
-x = BatchNormalization()(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPooling2D((2, 2))(x)
+# Transform target variable into one-hotencoding
+y_cat_train = to_categorical(y_train, 10)
+y_cat_test = to_categorical(y_test, 10)
 
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPooling2D((2, 2))(x)
+print(y_cat_train)
 
-x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPooling2D((2, 2))(x)
+model = Sequential()
 
-x = Flatten()(x)
-x = Dropout(0.2)(x)
+# Convolutional Layer
+model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+# Pooling layer
+model.add(MaxPool2D(pool_size=(2, 2)))
+# Dropout layers
+model.add(Dropout(0.25))
 
-# Hidden layer
-x = Dense(1024, activation='relu')(x)
-x = Dropout(0.2)(x)
+model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-# last hidden layer i.e.. output layer
-x = Dense(K, activation='softmax')(x)
+model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model = Model(i, x)
+model.add(Flatten())
+# model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(10, activation='softmax'))
 
-# model description
+METRICS = [
+    'accuracy',
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall')
+]
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=METRICS)
+
 model.summary()
+
+early_stop = EarlyStopping(monitor='val_loss', patience=2)
+
+batch_size = 32
+data_generator = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+train_generator = data_generator.flow(X_train, y_cat_train, batch_size)
+steps_per_epoch = X_train.shape[0] // batch_size
+
+r = model.fit(train_generator,
+              epochs=50,
+              steps_per_epoch=steps_per_epoch,
+              validation_data=(X_test, y_cat_test),
+#               callbacks=[early_stop],
+#               batch_size=batch_size,
+             )
+
+plt.figure(figsize=(12, 16))
+
+plt.subplot(4, 2, 1)
+plt.plot(r.history['loss'], label='Loss')
+plt.plot(r.history['val_loss'], label='val_Loss')
+plt.title('Loss Function Evolution')
+plt.legend()
+
+plt.subplot(4, 2, 2)
+plt.plot(r.history['accuracy'], label='accuracy')
+plt.plot(r.history['val_accuracy'], label='val_accuracy')
+plt.title('Accuracy Function Evolution')
+plt.legend()
+
+plt.subplot(4, 2, 3)
+plt.plot(r.history['precision'], label='precision')
+plt.plot(r.history['val_precision'], label='val_precision')
+plt.title('Precision Function Evolution')
+plt.legend()
+
+plt.subplot(4, 2, 4)
+plt.plot(r.history['recall'], label='recall')
+plt.plot(r.history['val_recall'], label='val_recall')
+plt.title('Recall Function Evolution')
+plt.legend()
