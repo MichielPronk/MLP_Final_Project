1858fcf68b9d2247c102ca42a727bdaf07332997
jlogtenberg <j.logtenberg@student.rug.nl>
2022-03-24 12:15:54 +0100

    Model code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import PIL
 
 # other imports
 import numpy as np
@@ -14,3 +15,46 @@ cifar10 = tf.keras.datasets.cifar10
 # Distribute it to train and test set
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+# number of classes
+K = len(set(y_train))
+
+# calculate total number of classes
+# for output layer
+print("number of classes:", K)
+
+# Build the model using the functional API
+# input layer
+i = Input(shape=x_train[0].shape)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(i)
+x = BatchNormalization()(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+
+# Hidden layer
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.2)(x)
+
+# last hidden layer i.e.. output layer
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
+
+# model description
+model.summary()
